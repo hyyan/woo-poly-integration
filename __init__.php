8a37408d317f647b858af0ad522fdf3a09898742
Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
2015-03-20 23:04:00 -0700

    Fixed a typo

--- __init__.php
+++ __init__.php
@@ -12,7 +12,7 @@
  */
 
 /**
- * This file is part of the hyyan/woo-poly-integration plubin.
+ * This file is part of the hyyan/woo-poly-integration plugin.
  * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
--- src/WPI/Plugin.php
+++ src/WPI/Plugin.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of the hyyan/woo-poly-integration plubin.
+ * This file is part of the hyyan/woo-poly-integration plugin.
  * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
