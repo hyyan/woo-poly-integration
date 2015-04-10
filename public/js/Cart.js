ddd2522010df19bfbd6e903feeb7a3da9a7c9929
Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
2015-04-09 18:53:01 -0700

    Added header

--- public/js/Cart.js
+++ public/js/Cart.js
@@ -1,4 +1,12 @@
-/** 
+/*
+ * This file is part of the hyyan/woo-poly-integration plugin.
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/*
  * Modified cart-fragments.js script to break HTML5 fragment caching. 
  * Useful with WPML when switching languages 
  * 
