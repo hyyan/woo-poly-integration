68bc57450c6b00ab4cba1b86fded66f98535e8c5
Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
2015-04-09 18:51:58 -0700

    Refactor FieldsLocker.js

--- public/js/FieldsLocker.js
+++ public/js/FieldsLocker.js
@@ -1,95 +1,98 @@
-jQuery(document).ready(function ($) {
-    var i;
-    var ids = [
-        '_virtual'
-                , '_downloadable'
-                , '_backorders'
-                , 'product-type'
-                , '_manage_stock'
-                , '_stock'
-                , '_sold_individually'
-                , 'comment_status'
-                , '_tax_status'
-                , '_tax_class'
-                , 'parent_id'
-                , '_stock_status'
-                //, 'crosssell_ids'
-                //, 'upsell_ids'
-    ];
+/*
+ * This file is part of the hyyan/woo-poly-integration plugin.
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-    for (i = 0; i < ids.length; i++) {
-        $('#' + ids[i]).attr('disabled', 'disabled');
-    }
+(function ($) {
 
-    var inpt_names = [
-        '_width'
-                , '_height'
-                , '_sku'
-                , '_length'
-                , '_weight'
-                , 'product_length'
-                , '_regular_price'
-                , '_sale_price'
-                , '_sale_price_dates_from'
-                , '_sale_price_dates_to'
-                , 'menu_order'
-    ];
-    for (i = 0; i < inpt_names.length; i++) {
-        $('input[name="' + inpt_names[i] + '"]').attr('readonly', 'readonly');
+    /**
+     * Constrcut the class
+     * 
+     * @param {jQuery} $ jquery object
+     * @returns {FieldsLocker_L1.FieldsLocker}
+     */
+    var FieldsLocker = function ($) {
+        this.$ = $;
+    };
 
-        //variation fields
-        $('input[name^="variable' + inpt_names[i] + '"]').each(function () {
-            $(this).attr('readonly', 'readonly');
-        });
-    }
+    FieldsLocker.prototype = {
+        /**
+         * Constrcutor
+         */
+        constructor: FieldsLocker,
+        /**
+         * Do lock all fields
+         */
+        start: function () {
 
-    var var_checkboxes = [
-        '_enabled'
-                , '_is_downloadable'
-                , '_is_virtual'
-                , '_manage_stock'
-    ];
-    for (i = 0; i < var_checkboxes.length; i++) {
-        $('input[name^="variable' + var_checkboxes[i] + '"]').each(function () {
-            $(this).attr('readonly', 'readonly');
-        });
-    }
+            // handle disabled items
+            var disabled = this.getDisabledItems();
+            for (var i = 0; i < disabled.length; i++) {
+                $(disabled[i]).attr('disabled', 'disabled');
+            }
 
-
-    var var_selectboxes = ['_stock_status', '_shipping_class', '_tax_class'];
-    for (i = 0; i < var_selectboxes.length; i++) {
-        $('select[name^="variable' + var_selectboxes[i] + '"]').each(function () {
-            $(this).attr('disabled', 'disabled');
-        });
-    }
-
-
-    $('.woocommerce_attribute_data td textarea,.attribute_values').each(function () {
-        $(this).attr('readonly', 'readonly');
-    });
-
-
-    $('.woocommerce_variation>h3 select, #variable_product_options .toolbar select, .woocommerce_attribute_data input[type="checkbox"]').each(function () {
-        $(this).attr('disabled', 'disabled');
-    });
-
-    $('form#post input[type="submit"]').click(function () {
-        for (i = 0; i < ids.length; i++) {
-            $('#' + ids[i]).removeAttr('disabled');
+            // handle deleted items
+            var deleted = this.getDeletedItems();
+            for (var i = 0; i < deleted.length; i++) {
+                $(deleted[i]).css({visibility: 'hidden'});
+            }
         }
-        $('.woocommerce_variation select,#variable_product_options .toolbar select,.woocommerce_variation input[type="checkbox"],.woocommerce_attribute_data input[type="checkbox"]').each(function () {
-            $(this).removeAttr('disabled');
-        });
+
+        /**
+         * Get array of selectors to disable
+         * 
+         * @returns Array
+         */
+        , getDisabledItems: function () {
+            // default ids
+            var items = [
+                // product types
+                '.type_box *',
+                // inputs : general
+                '#general_product_data *',
+                // inputs : stock
+                '#inventory_product_data *',
+                // inputs : shipping
+                '#shipping_product_data *',
+                // inputs : related
+                '#parent_id',
+                // inputs : attributes
+                '#product_attributes *',
+                // inputs : advanced setting
+                '#comment_status',
+                '#menu_order'
+            ];
+
+            // fire event to allow other plugins to extend the list
+            this.$('body').trigger('getDisabledItems.Hyyan_WPI', [items]);
+
+            return items;
+        }
+
+        /**
+         * Get array of selectors to delete
+         * 
+         * @returns {Array}
+         */
+        , getDeletedItems: function () {
+            // default items
+            var items = [
+                '#general_product_data .delete'
+            ];
+
+            // fire event to allow other plugins to extend the list
+            this.$('body').trigger('getDeletedItems.Hyyan_WPI', [items]);
+
+            return items;
+        }
+    };
+
+    // bootstrap 
+    $(document).ready(function () {
+        new FieldsLocker($).start();
     });
 
-
-    //quick edit fields
-    for (i = 0; i < ids.length; i++) {
-        $('.inline-edit-product [name="' + ids[i] + '"]').attr('disabled', 'disabled');
-    }
-
-    for (i = 0; i < inpt_names.length; i++) {
-        $('.inline-edit-product [name="' + inpt_names[i] + '"]').attr('readonly', 'readonly');
-    }
-
-});
+})(window.jQuery);
