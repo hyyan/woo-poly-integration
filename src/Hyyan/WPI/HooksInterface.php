54287fcc292aa0355f392c9c48a0a6670323d047
Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
2015-04-11 21:32:23 -0700

    Created Hooks Interface

---
+++ src/Hyyan/WPI/HooksInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of the hyyan/woo-poly-integration plugin.
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Hyyan\WPI;
+
+/**
+ * Plugin Hooks Interface
+ *
+ * @author Hyyan
+ */
+interface HooksInterface
+{
+
+}
