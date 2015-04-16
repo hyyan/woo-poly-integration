b11a2b9bbfa08b391785421bb3397fd1e0672f08
Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
2015-04-16 00:48:54 -0700

    Added Utilities Class

---
+++ src/Hyyan/WPI/Utilities.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * This file is part of the hyyan/woo-poly-integration plugin.
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Hyyan\WPI;
+
+/**
+ * Utilities
+ *
+ * Some helper methods
+ *
+ * @author Hyyan
+ */
+final class Utilities
+{
+
+    /**
+     * Get the translations IDS of the given product ID
+     *
+     * @global \Polylang $polylang
+     *
+     * @param integer $ID             the product ID
+     * @param boolean $excludeDefault ture to exclude defualt language
+     *
+     * @return array associative array with language code as key and ID of translations
+     *               as value.
+     */
+    public static function getTranslationsArrayByID($ID, $excludeDefault = false)
+    {
+        global $polylang;
+        $IDS = $polylang->get_translations('post', $ID);
+        if (true === $excludeDefault) {
+            unset($IDS[pll_default_language()]);
+        }
+
+        return $IDS;
+    }
+
+    /**
+     * Get the translations IDS of the given product object
+     *
+     * @see \Hyyan\WPI\getProductTranslationsByID()
+     *
+     * @param \WC_Product $product        the product object
+     * @param boolean     $excludeDefault ture to exclude defualt language
+     *
+     * @return array associative array with language code as key and ID of translations
+     *               as value.
+     */
+    public static function getTranslationsArrayByObject(\WC_Product $product, $excludeDefault = false)
+    {
+        return getProductTranslationsByID($product->id, $excludeDefault);
+    }
+
+    /**
+     * Get porduct translation by ID
+     *
+     * @param integer $ID   the porduct ID
+     * @param string  $slug the language slug
+     *
+     * @return \WC_Product|false product translation if found , false if the
+     *                           given ID is not for product
+     */
+    public static function getProductTranslationByID($ID, $slug = '')
+    {
+        $product = wc_get_product($ID);
+        if (!$product) {
+            return false;
+        }
+
+        return getProductTranslationByObject($product, $slug);
+    }
+
+    /**
+     * Get product translation by object
+     *
+     * @param \WC_Product $product the product to use to retirve translation
+     * @param string      $slug    the language slug
+     *
+     * @return \WC_Product product translation or same prodcut if translaion not found
+     */
+    public static function getProductTranslationByObject(\WC_Product $product, $slug = '')
+    {
+        $productTranslationID = pll_get_post($product->id, $slug);
+
+        if ($productTranslationID) {
+            $translated = wc_get_product($productTranslationID);
+            $product = $translated ? $translated : $product;
+        }
+
+        return $product;
+    }
+
+}
