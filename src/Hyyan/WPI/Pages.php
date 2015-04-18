80e35ad129fe27bbc22917400375dc6a7e667fb6
Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
2015-04-18 03:29:58 -0700

    Added "correctShopPage" method

--- src/Hyyan/WPI/Pages.php
+++ src/Hyyan/WPI/Pages.php
@@ -39,6 +39,10 @@ class Pages
             add_filter(sprintf('woocommerce_get_%s_page_id', $page), $method);
             add_filter(sprintf('option_woocommerce_%s_page_id', $page), $method);
         }
+
+        if (!is_admin()) {
+            add_filter('parse_request', array($this, 'correctShopPage'));
+        }
     }
 
     /**
@@ -59,4 +63,35 @@ class Pages
         return $id;
     }
 
+    /**
+     * Correct the shop page to display products from currrent language only
+     *
+     * @param  \WP     $wp wordpress instance
+     * @return boolean false if the current language is the same as default
+     *                    language or if the "pagename" var is empty
+     */
+    public function correctShopPage(\WP $wp)
+    {
+        if (pll_default_language() === pll_current_language()) {
+            return false;
+        }
+
+        if (empty($wp->query_vars['pagename'])) {
+            return false;
+        }
+
+        $shopPage = get_post(wc_get_page_id('shop'));
+
+        /* Explode by / for children page */
+        $page = explode('/', $wp->query_vars['pagename']);
+
+        if (
+                isset($shopPage->post_name) &&
+                $shopPage->post_name == $page[count($page) - 1]
+        ) {
+            unset($wp->query_vars['page']);
+            unset($wp->query_vars['pagename']);
+            $wp->query_vars['post_type'] = 'product';
+        }
+    }
 }
