b3fc0eec6532d177acccb1a751ee6fc5aea676bd
Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
2015-04-26 10:41:41 -0700

    Fixed header

--- src/Hyyan/WPI/Autoloader.php
+++ src/Hyyan/WPI/Autoloader.php
@@ -1,9 +1,11 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * This file is part of the hyyan/woo-poly-integration plugin.
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Hyyan\WPI;
