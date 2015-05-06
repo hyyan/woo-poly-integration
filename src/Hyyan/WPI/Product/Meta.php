eafce04b26b4d188a20f470d38621e452e72fd6c
Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
2015-05-05 19:50:22 -0700

    Readded "defineProductMetaToCopy" as public method

--- src/Hyyan/WPI/Product/Meta.php
+++ src/Hyyan/WPI/Product/Meta.php
@@ -43,54 +43,7 @@ class Meta
     {
 
         // sync product meta with polylang
-        add_filter('pll_copy_post_metas', function ($metas) {
-            $default = array(
-                // general
-                'product-type',
-                '_virtual',
-                '_downloadable',
-                '_sku',
-                '_regular_price',
-                '_sale_price',
-                '_sale_price_dates_from',
-                '_sale_price_dates_to',
-                '_downloadable_files',
-                '_download_limit',
-                '_download_expiry',
-                '_download_type',
-                // stock
-                '_manage_stock',
-                '_stock',
-                '_backorders',
-                '_stock_status',
-                '_sold_individually',
-                // shipping
-                '_weight',
-                '_length',
-                '_width',
-                '_height',
-                'product_shipping_class',
-                // advanced
-                '_purchase_note',
-                'menu_order',
-                'comment_status',
-                // extra
-                '_upsell_ids',
-                '_crosssell_ids',
-                '_featured',
-                '_thumbnail_id',
-                '_price',
-                '_product_image_gallery',
-                'total_sales',
-                '_translation_porduct_type',
-                '_visibility',
-            );
-
-            return array_merge(
-                    $metas
-                    , apply_filters(HooksInterface::PRODUCT_META_SYNC_FILTER, $default)
-            );
-        });
+        add_filter('pll_copy_post_metas', array($this, 'defineProductMetaToCopy'));
 
         $currentScreen = get_current_screen();
 
@@ -130,6 +83,65 @@ class Meta
 
         /* sync selected prodcut type */
         $this->syncSelectedProdcutType($ID);
+    }
+
+    /**
+     * Define the meta keys that must copyied from orginal product to its
+     * translation
+     *
+     * @param array $metas array of meta keys
+     *
+     * @return array extended meta keys array
+     */
+    public function defineProductMetaToCopy(array $metas = array())
+    {
+
+        $default = array(
+            // general
+            'product-type',
+            '_virtual',
+            '_downloadable',
+            '_sku',
+            '_regular_price',
+            '_sale_price',
+            '_sale_price_dates_from',
+            '_sale_price_dates_to',
+            '_downloadable_files',
+            '_download_limit',
+            '_download_expiry',
+            '_download_type',
+            // stock
+            '_manage_stock',
+            '_stock',
+            '_backorders',
+            '_stock_status',
+            '_sold_individually',
+            // shipping
+            '_weight',
+            '_length',
+            '_width',
+            '_height',
+            'product_shipping_class',
+            // advanced
+            '_purchase_note',
+            'menu_order',
+            'comment_status',
+            // extra
+            '_upsell_ids',
+            '_crosssell_ids',
+            '_featured',
+            '_thumbnail_id',
+            '_price',
+            '_product_image_gallery',
+            'total_sales',
+            '_translation_porduct_type',
+            '_visibility',
+        );
+
+        return array_merge(
+                $metas
+                , apply_filters(HooksInterface::PRODUCT_META_SYNC_FILTER, $default)
+        );
     }
 
     /**
@@ -196,4 +208,5 @@ class Meta
             );
         }, 100);
     }
+
 }
