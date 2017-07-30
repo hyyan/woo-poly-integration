8b7772df7470d352f848bf08f23071ef1726e3d5
Hyyan Abo Fakher <hyyanaf@gmail.com>
2017-07-30 02:19:11 +0200

    improve deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,23 @@
 #!/usr/bin/env bash
-
-WP_ORG_USERNAME="hyyan"
-PLUGIN="woo-poly-integration"
-PLUGIN_BUILDS_PATH="./build"
-  
-cd "$PLUGIN_BUILDS_PATH"
-rm -rf *
+rm -rf build
 
 # prepare files to upload
-rsync -av . $PLUGIN_BUILDS_PATH --exclude={build,assets,nbproject,*.json,*.sh,*.png,.*}
+rsync -avh . ./build --exclude-from '.exclude-list' --delete
+
+# change to build
+cd build
 
 # Checkout the SVN repo
-svn co -q "http://svn.wp-plugins.org/$PLUGIN" svn
+svn co -q "http://svn.wp-plugins.org/woo-poly-integration" svn
 
 # Move out the trunk directory to a temp location
-mv svn/trunk ./svn-trunk
+mv ./svn/trunk ./svn-trunk
 
 # Create trunk directory
-mkdir svn/trunk
+mkdir -p svn/trunk
 
 # Copy our new version of the plugin into trunk
-rsync -r -p $PLUGIN_BUILDS_PATH/* svn/trunk
+rsync -r -p ./* svn/trunk
 
 # Copy all the .svn folders from the checked out copy of trunk to the new trunk.
 # This is necessary as the Travis container runs Subversion 1.6 which has .svn dirs in every sub dir
@@ -48,8 +45,8 @@ cd ../
 rm -fR svn-trunk
 
 # Add new version tag
-mkdir svn/tags/$TRAVIS_TAG
-rsync -r -p $PLUGIN_BUILDS_PATH/* svn/tags/$TRAVIS_TAG
+mkdir -p ./svn/tags/$TRAVIS_TAG
+rsync -r -p . svn/tags/$TRAVIS_TAG
 
 # Add new files to SVN
 svn stat svn | grep '^?' | awk '{print $2}' | xargs -I x svn add x@
@@ -58,5 +55,5 @@ svn stat svn | grep '^!' | awk '{print $2}' | xargs -I x svn rm --force x@
 svn stat svn
 
 # Commit to SVN
-svn ci --no-auth-cache --username $WP_ORG_USERNAME --password $WP_ORG_PASSWORD svn -m "Deploy version $VERSION"
+svn ci --no-auth-cache --username hyyan --password $WP_ORG_PASSWORD svn -m "build version $TRAVIS_TAG"
 
