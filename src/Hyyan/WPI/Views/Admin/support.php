6a8a39815a91130f70e55e38d218e32250bc9bc6
mrleemon <tacrapo@gmail.com>
2018-10-10 17:08:37 +0200

    Update support.php

--- src/Hyyan/WPI/Views/Admin/support.php
+++ src/Hyyan/WPI/Views/Admin/support.php
@@ -10,16 +10,16 @@ if (!defined('ABSPATH')) {
 </h3>
 <div class="inside">
     <?php
-    _e('<strong>I will never ask you for donation , now or in the future</strong> ,
-        but the plugin stills need your support.
-        please support by rating this plugin On
-        <a href="https://wordpress.org/support/view/plugin-reviews/woo-poly-integration">Wordpress Repository</a> ,
+    _e('<strong>I will never ask you for donation, now or in the future</strong>,
+        but the plugin still needs your support.
+        please support by rating this plugin on
+        <a href="https://wordpress.org/support/view/plugin-reviews/woo-poly-integration">Wordpress Repository</a>,
         or by giving the plugin a star on  <a href="https://github.com/hyyan/woo-poly-integration">Github</a>.
         <br><br>
-        If you speak langauge other than English ,
+        If you speak a language other than English,
         you can support the plugin by extending the
-        trasnlation list. and your name will be added
-        to translators list', 'woo-poly-integration'
+        translation list and your name will be added
+        to the translators list', 'woo-poly-integration'
     );
     ?>
     <br><br>
